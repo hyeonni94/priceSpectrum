b1921b7880bf50bc2d258690ae4e27997f7b4a6c
wooyol <neolaocoon@naver.com>
2017-03-04 19:18:27 +0900

    sampling.py change

--- sampling.py
+++ sampling.py
@@ -6,7 +6,7 @@ import pandas as pd
 json_key = "pricespectrum-ddb205dae4ad.json"
 client = get_client(json_key_file=json_key, readonly=True)
 
-sql = "SELECT delngDe as Date, aucSeNm as TradeType, whsalMrktNewNm as Market, insttNewNm as Agent, shipmntSeNm as ShippingType, stdMtcNewNm as Origin, stdSpciesNm as Species, stdFrmlcNewNm as Package, delngPrut as Unit, stdQlityNewNm as Grade, delngQy as Quantity, sbidPric as Price FROM [pricespectrum-159700:auction_price.realtime] WHERE stdPrdlstNewCode = '0909' AND RAND() < 0.05 LIMIT 1000"
+sql = "SELECT delngDe as Date, aucSeNm as TradeType, whsalMrktNewNm as Market, insttNewNm as Agent, shipmntSeNm as ShippingType, stdMtcNewNm as Origin, stdSpciesNm as Species, stdFrmlcNewNm as Package, delngPrut as Unit, stdQlityNewNm as Grade, delngQy as Quantity, sbidPric as Price, INTEGER(CEIL(sbidPric / FLOAT(delngPrut))) as UnitPrice FROM  [pricespectrum-159700:auction_price.realtime] WHERE stdPrdlstNewCode = '0909' AND RAND() < 0.05 LIMIT 1000"
 
 job_id, _results = client.query(sql)
 
@@ -21,4 +21,3 @@ results = client.get_query_rows(job_id)
 sample = pd.DataFrame(results)
 
 sample.to_csv("samples/sample.csv", header = True, index = False, encoding = "utf-8")
-
